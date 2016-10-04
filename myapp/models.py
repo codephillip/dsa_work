1a315ea0b1707e1cf749be3120eedffb547c990b
codephillip <codephillip@gmail.com>
2016-10-04 13:19:20 +0300

    added Topic and Subtopic to models.py, registered models in admin.py

--- myapp/models.py
+++ myapp/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.name
+
+
+class SubTopic(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    description = models.TextField()
+    topic = models.ForeignKey(Topic)
+
+    def __unicode__(self):
+        return self.name
