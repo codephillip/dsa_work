1903b962d947c986f06e48b64cf2741603171f9a
codephillip <codephillip@gmail.com>
2016-10-04 11:39:05 +0300

    initial commit

---
+++ dsawork/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dsawork project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dsawork.settings")
+
+application = get_wsgi_application()
