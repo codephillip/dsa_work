ff41de324009eb1b8463b5b2a24ab9a1e2321fc6
codephillip <codephillip@gmail.com>
2016-10-06 13:13:27 +0300

    added member to model and added names

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from myapp.models import Topic, SubTopic
+from myapp.models import Topic, SubTopic, Member
 
 admin.site.register(Topic)
 admin.site.register(SubTopic)
+admin.site.register(Member)
--- myapp/models.py
+++ myapp/models.py
@@ -14,9 +14,19 @@ class Topic(models.Model):
 
 class SubTopic(models.Model):
     name = models.CharField(max_length=200)
-    image = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, null=True, default=None)
     description = models.TextField()
     topic = models.ForeignKey(Topic)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, null=True, default="images/te1.jpg")
+    reg_num = models.CharField(max_length=20)
+    student_num = models.IntegerField(null=True, default=12345)
 
     def __unicode__(self):
         return self.name
