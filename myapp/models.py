9c492d5b88dcaa00fd3398e426c1c90d61559b12
codephillip <codephillip@gmail.com>
2016-10-06 15:46:37 +0300

    added default image, added content

--- myapp/models.py
+++ myapp/models.py
@@ -14,7 +14,7 @@ class Topic(models.Model):
 
 class SubTopic(models.Model):
     name = models.CharField(max_length=200)
-    image = models.CharField(max_length=200, null=True, default=None)
+    image = models.CharField(max_length=200, null=True, default="images/dsa1.png")
     description = models.TextField()
     topic = models.ForeignKey(Topic)
 
@@ -24,7 +24,7 @@ class SubTopic(models.Model):
 
 class Member(models.Model):
     name = models.CharField(max_length=200)
-    image = models.CharField(max_length=200, null=True, default="images/te1.jpg")
+    image = models.CharField(max_length=200, null=True, default="images/dsa1.png")
     reg_num = models.CharField(max_length=20)
     student_num = models.IntegerField(null=True, default=12345)
 
